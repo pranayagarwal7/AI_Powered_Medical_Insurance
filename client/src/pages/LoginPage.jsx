0d51c354e39b71b58a6aeb954b804072986db2ff
Ryan <ryan.cuthbert12@gmail.com>
2025-12-03 14:41:20 -0500

    created login function

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,15 +1,60 @@
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 function LoginPage(){
+    const [verifyUser, setVerifyUser] = useState({
+            email: "",
+            password: ""
+    
+        })
+    
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+
+        setVerifyUser({
+            ...verifyUser,
+            [name]: value
+        })
+    }
+
+    const handleVerifyUser = (e) => {
+        e.preventDefault()
+
+        try {
+            const users = JSON.parse(localStorage.getItem("users") || "[]")
+            const existingUser = users.find((u) => u.email === verifyUser.email)
+
+            if(existingUser){
+                setIsLoggedIn(true)
+            } else{
+                alert("User does not exist. Please create an account")
+                setIsLoggedIn
+            }
+            
+        } catch (error) {
+            console.error(error)
+            setIsLoggedIn(false)
+        }
+    }
+
+    useEffect(() => {
+        console.log("isLoggedIn: ", isLoggedIn)
+    }, [isLoggedIn])
     return(
          <section className="account-form-section">
             <h1>Medical Insurance Login</h1>
             <p>Welcome back. Sign in to continue.</p>
-            <form>
+            <form onSubmit={handleVerifyUser}>
                 <div className="input-div">
                     <label htmlFor="email">Email</label>
                     <input 
                     type="email" 
                     name="email"
+                    id="email"
+                    value={verifyUser.email}
+                    onChange={handleChange}
+                    required
                     />
                 </div>
                 <div className="input-div">
@@ -17,6 +62,10 @@ function LoginPage(){
                     <input 
                     type="password" 
                     name="password"
+                    id="password"
+                    value={verifyUser.password}
+                    onChange={handleChange}
+                    required
                     />
                     <input type="submit" value={"Create Account"}/>
                     <p>Don't have an account? <Link to={"/signup"}>Create account</Link></p>
